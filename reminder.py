e2420381dfcee860bfda278991063bcf7ecf21fb
AlexanderF048 <113266989+AlexanderF048@users.noreply.github.com>
2022-09-25 02:10:31 +0300

    updated up to advises with additional code
     if current_datetime <= date <= end_week or (current_datetime.isocalendar()[2] == 1 and current_datetime-datetime.timedelta(days=2)  <= date <= current_datetime):

--- reminder.py
+++ reminder.py
@@ -1,66 +1,29 @@
-#ФУНКЦИЯ выводит данные о днях рождениях на ТЕКУЩЕЙ неделе, если она вызвана в понедельник, в том числе отображаются именинники на прошедших выходных(дополнительным блоком).
+#ФУНКЦИЯ выводит данные о днях рождениях на 7 дней, если она вызвана в понедельник, в том числе отображаются именинники на прошедших выходных(дополнительным блоком).
 import datetime
 
 
-def get_birthdays_per_week(birhtsday_list):
-   
-    monday=[]
-    tuesday=[]
-    wednesday=[]
-    thusday=[]
-    friday=[]
-    saturday=[]
-    sunday=[]
-    
-
-    birthsdays={"Monday":monday, "Tuesday":tuesday, "Wednesday":wednesday, "Thursday":thusday, "Friday":friday, "Saturday":saturday, "Sunday":sunday}
-    
-    week_days={1:"Monday",2:"Tuesday", 3:"Wednesday",4:"Thursday",5:"Friday",6:"Saturday",7:"Sunday"}
+def get_birthdays_per_week(birhtsday_list):  
+      
+    birthsdays={"Monday":[], "Tuesday":[], "Wednesday":[], "Thursday":[], "Friday":[]}
+    week_days={1:"Monday",2:"Tuesday", 3:"Wednesday",4:"Thursday",5:"Friday"}
  
     current_datetime = datetime.date.today() 
-    ##current_datetime = datetime.date(2022,9,26) #Тестовая дата
-    current_datetime_wday = current_datetime.weekday() # Mon====0
-    
-  
-    start_week = current_datetime - datetime.timedelta(days=current_datetime_wday)
-    end_week = start_week + datetime.timedelta(days=4)
+    print(current_datetime)
+        
+    end_week = current_datetime + datetime.timedelta(days=7)
  
-
     for name, date in birhtsday_list.items():  
 
         date= date.replace(year=current_datetime.isocalendar()[0]) #Приводим год к нынешнему
-        #print(date)
-
-        if start_week <= date <= end_week:
-            
-            day=week_days[date.isocalendar()[2]]
+        
+        if current_datetime <= date <= end_week or (current_datetime.isocalendar()[2] == 1 and current_datetime-datetime.timedelta(days=2)  <= date <= current_datetime):
+            day = week_days.get(date.isocalendar()[2], 'Monday') #возвращает значение для ключа key, если ключ находится в словаре, если ключ отсутствует то вернет значение default "Monday"
             birthsdays[day].append(name)
           
-        #------- weekend birthsdays-------
-        if current_datetime_wday == 0:
-            marker_l_sun=current_datetime-datetime.timedelta(days=1)
-            marker_l_sat=current_datetime-datetime.timedelta(days=2)    
-
-            if date == marker_l_sun or date == marker_l_sat:
-                monday.append(name)
-            
-    
-    #OUTPUT-----"THIS WEEK"--------------------
     print("-----------------------------------------------------\nOUTPUT:\n")
     for day, person in birthsdays.items():
         
         if birthsdays[day]:
             print(f"{day}:{','.join(birthsdays[day])}") 
-    #------------------------------
-    
-   
-    #print(birthsdays)
+
     return 
-
-
-
-
-#birhtsday_list={"Daria":datetime.date(year=1990, month=9, day=24), "Pavel":datetime.date(year=1990, month=9, day=28), "Natali":datetime.date(year=1990, month=9, day=25), "Sam":datetime.date(year=1990, month=9, day=29)}
-#print("input data dic:",birhtsday_list)
-
-#get_birthdays_per_week(birhtsday_list)
